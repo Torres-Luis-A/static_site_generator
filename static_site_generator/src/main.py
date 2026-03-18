3300b493bb02aa24f826d37e5a0d732da2a36927
Torres-Luis-A <torresla960@gmail.com>
2026-03-18 12:40:30 -0400

    implement main function and enhance TextNode class with additional text types

--- static_site_generator/src/main.py
+++ static_site_generator/src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode
+
+def main():
+    textNode = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+
+    print(textNode)
+if __name__ == "__main__":
+    main()
--- static_site_generator/src/textnode.py
+++ static_site_generator/src/textnode.py
@@ -1,6 +1,21 @@
 from enum import Enum
 
 class TexType(Enum):
-    PLAIN_TEXT = 1
-    LINK = 2
-    IMAGE = 4
+    plain_text = 1
+    bold_text = 2
+    code_text = 3
+    link_text = 4
+    images_text = 5
+
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+        
+    def __eq__(self, other):
+        if self == other:
+            return True
+        
+    def __repr__(TEXT, TEXT_TYPE, URL):
+        return f"TextNode(text={TEXT}, text_type={TEXT_TYPE}, url={URL})"
