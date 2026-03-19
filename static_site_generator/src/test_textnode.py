0094fe88d9b254af20ba07d95a23aeab94c9f0fc
Torres-Luis-A <torresla960@gmail.com>
2026-03-19 10:43:31 -0400

    refactor test_eq method in TestTextNode class to include additional assertions for TextNode equality

--- static_site_generator/src/test_textnode.py
+++ static_site_generator/src/test_textnode.py
@@ -7,7 +7,16 @@ class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD,)
         node2 = TextNode("This is a text node", TextType.BOLD,)
-        self.assertEqual(node, node2)
+        node3 = TextNode("This is a text node", TextType.ITALIC,)
+        node4 = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+        node5 = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+        node6 = TextNode("This is a text node", TextType.BOLD, None)
+        node7 = TextNode("This is a text node", TextType.BOLD,)
+        self.assertEqual(node, node)
+        self.assertNotEqual(node, node3)
+        self.assertEqual(node5, node5)
+        self.assertEqual(node6,node7)
+
 
 
 if __name__ == "__main__":
