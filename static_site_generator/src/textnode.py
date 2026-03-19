f5e76ae00b9e6823fe0bcf35a7133e859e95fa3d
Torres-Luis-A <torresla960@gmail.com>
2026-03-19 10:01:33 -0400

    add unit tests for TextNode class and fix TextType enum definition

--- static_site_generator/src/textnode.py
+++ static_site_generator/src/textnode.py
@@ -1,21 +1,24 @@
 from enum import Enum
 
-class TexType(Enum):
-    plain_text = 1
-    bold_text = 2
-    code_text = 3
-    link_text = 4
-    images_text = 5
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 class TextNode:
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
         
     def __eq__(self, other):
-        if self == other:
-            return True
-        
-    def __repr__(TEXT, TEXT_TYPE, URL):
-        return f"TextNode(text={TEXT}, text_type={TEXT_TYPE}, url={URL})"
+        return(
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
+    def __repr__(self):
+        return f"TextNode(text={self.text}, text_type={self.text_type}, url={self.url})"
