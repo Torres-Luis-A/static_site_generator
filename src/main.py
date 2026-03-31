653794fe1818a3984413d4e7148c114281f6b4fd
Torres-Luis-A <torresla960@gmail.com>
2026-03-30 22:22:56 -0400

    feat: restructure project by removing unused HTML and CSS files; add static file copying functionality

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
-from textnode import TextNode
+import os
+import shutil
+
+from copy_static import copy_static_to_public
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
 
 def main():
-    textNode = TextNode("This is some anchor text", "bold", "https://www.boot.dev")
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    print(textNode)
-if __name__ == "__main__":
-    main()
+    print("Copying static files to public directory...")
+    copy_static_to_public(dir_path_static, dir_path_public)
+
+
+main()
